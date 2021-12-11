38ee62352151be20a74852ba43ff2fd208a7826f
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-11 15:11:05 -0500

    complete module 20

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-var greet = require('./greet');
+// var greet = require('./greet');
 // greet.greet();
 
-var person = {
-    firstname: 'John',
-    lastname: 'Doe',
-    greet: function() {
-        console.log('Hello ' + this.firstname + ' ' + this.lastname);
-    }
+// var person = {
+//     firstname: 'John',
+//     lastname: 'Doe',
+//     greet: function() {
+//         console.log('Hello ' + this.firstname + ' ' + this.lastname);
+//     }
+// };
+
+// person.greet();
+// console.log(person.firstname);
+// console.log(person['lastname']);
+// person['greet']();
+
+function Person(firstname, lastname) {
+    this.fistname = firstname;
+    this.lastname = lastname;
+}
+
+Person.prototype.greet = function() {
+    console.log('My name is ' + this.fistname + ' ' + this.lastname);
+}
+
+var person = new Person('John', 'Doe');
+console.log('Hi ' + person.fistname + ' ' + person.lastname);
+console.log(person);
+console.log(person.__proto__);
+person.greet();
+
+var another = {
+    firstname: 'Peter',
+    lastname: 'Pan'
 };
 
-person.greet();
-console.log(person.firstname);
-console.log(person['lastname']);
-person['greet']();
+var person2 = new Person('Jane', 'Doe');
+
+console.log(another);
+console.log(person.__proto__ === another.__proto__);
+console.log(person.__proto__ === person2.__proto__);
