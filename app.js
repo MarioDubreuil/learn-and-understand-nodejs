d3c75e537b6321d27d5c5c6fcb4fb0152f6cbf76
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-12 19:55:58 -0500

    complete module 22

--- app.js
+++ app.js
@@ -1,64 +1,17 @@
-// var greet = require('./greet');
-// greet.greet();
+console.log(firstname);
 
-// var person = {
-//     firstname: 'John',
-//     lastname: 'Doe',
-//     greet: function() {
-//         console.log('Hello ' + this.firstname + ' ' + this.lastname);
-//     }
-// };
+(function (lastname) {
+    var firstname = 'John';
+    console.log(firstname + ' ' + lastname);
+}('Doe'));
 
-// person.greet();
-// console.log(person.firstname);
-// console.log(person['lastname']);
-// person['greet']();
+function test() {
+    var firstname = 'Pete';
+    console.log(firstname);
+};
 
-// function Person(firstname, lastname) {
-//     this.fistname = firstname;
-//     this.lastname = lastname;
-// }
-
-// Person.prototype.greet = function() {
-//     console.log('My name is ' + this.fistname + ' ' + this.lastname);
-// }
-
-// var person = new Person('John', 'Doe');
-// console.log('Hi ' + person.fistname + ' ' + person.lastname);
-// console.log(person);
-// console.log(person.__proto__);
-// person.greet();
-
-// var another = {
-//     firstname: 'Peter',
-//     lastname: 'Pan'
-// };
-
-// var person2 = new Person('Jane', 'Doe');
-
-// console.log(another);
-// console.log(person.__proto__ === another.__proto__);
-// console.log(person.__proto__ === person2.__proto__);
-
-function changeInt(i) {
-    i = 2;
-}
-
-function changeObj(o) {
-    o.prop1 = function() {};
-    o.prop2 = {};
-}
-
-var i = 1;
-console.log(i);
-changeInt(i);
-console.log(i);
-
-var o = {};
-o.prop1 = {test: '1 2 testing'};
-console.log(o);
-changeObj(o);
-console.log(o);
-
-changeObj(i);
-console.log(i);
+console.log(firstname);
+var firstname = 'Jane';
+console.log(firstname);
+test();
+console.log(firstname);
