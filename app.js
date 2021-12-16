4f25503f048882b3cb624bb8697680db662d140f
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-16 18:51:46 -0500

    complete module 33

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
-var obj = {
-    greet : 'Hello'
-};
+var Emitter = require('./emitter');
 
-console.log(obj.greet);
-console.log(obj['greet']);
-var prop = 'greet';
-console.log(obj[prop]);
-console.log({greet: 'Hello2'});
-console.log({greet: 'Hello2'}.greet);
-console.log({greet: 'Hello2'}['greet']);
+var emitter = new Emitter();
 
-var arr = [];
-arr.push(123);
-arr.push('Yo!');
-arr.push({greet: 'Hello'});
-arr.push(function() {
-    console.log('Hello from function 1');
+emitter.on('greet', function() {
+    console.log('Someone, somewhere said hello.');
 });
-arr.push(function() {
-    console.log('Hello from function 2');
+
+emitter.on('greet', function() {
+    console.log('I whish you a good day.');
 });
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
-console.log(arr[4]);
-arr[3]();
-arr[4]();
-console.log('=====');
-arr.forEach(function(item) {
-    console.log();
-    console.log(typeof item);
-    console.log(item);
-    if (typeof item === 'function') {
-        item();
-    }
-});
+
+emitter.on('bye', function() {
+    console.log('Someone, somewhere said goodbye.')
+})
+
+console.log('Hello')
+emitter.emit('greet');
+console.log('bye');
+emitter.emit('bye');
