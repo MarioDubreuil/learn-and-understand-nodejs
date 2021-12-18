4c00765ebba8fe7ec3a68fbafb75607c2c196dbf
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-18 14:50:12 -0500

    complete module 40

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-var EventEmitter = require('events')
-var util = require('util')
+'use strict'
 
-function Greeter() {
-    EventEmitter.call(this)
-    this.gretting = 'Hello'
+function Person1(firstname, lastname) {
+    this.firstname = firstname
+    this.lastname = lastname
 }
 
-util.inherits(Greeter, EventEmitter)
-
-Greeter.prototype.greet = function(name) {
-    console.log(this.gretting + ' ' + name)
-    this.emit('greet', name)
+Person1.prototype.greet = function() {
+    console.log(`Hello1 ${this.firstname} ${this.lastname}`)
 }
 
-var greeter = new Greeter()
+var john1 = new Person1('John', 'Doe')
+john1.greet()
+ 
+var jane1 = new Person1('Jane', 'Doe')
+jane1.greet()
 
-greeter.on('greet', function(name) {
-    console.log('greeted ' + name)
-})
+console.log(john1.__proto__)
+console.log(jane1.__proto__)
+console.log(john1.__proto__ === jane1.__proto__)
 
-greeter.greet('Mario')
-
-function Person() {
-    this.firsname = 'John'
-    this.lastname = 'Doe'
+class Person2 {
+    constructor(firstname, lastname) {
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+    greet() {
+        console.log(`Hello2 ${this.firstname} ${this.lastname}`)
+    }
 }
 
-Person.prototype.greet = function() {
-    console.log('Hello ' + this.firsname + ' ' + this.lastname)
-}
+var john2 = new Person2('John', 'Doe')
+john2.greet();
+ 
+var jane2 = new Person2('Jane', 'Doe')
+jane2.greet()
 
-var john = new Person();
-john.greet();
-
-function Policeman() {
-    Person.call(this)
-    this.badgenumber = '12345'
-}
-
-util.inherits(Policeman, Person)
-
-var officer = new Policeman()
-
-officer.greet();
+console.log(john2.__proto__)
+console.log(jane2.__proto__)
+console.log(john2.__proto__ === jane2.__proto__)
