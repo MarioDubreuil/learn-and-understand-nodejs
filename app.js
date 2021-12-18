15100d22c9a3773ba300c0e62a2f6b4536b5adbf
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-18 10:34:44 -0500

    complete module 37

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-var EventEmitter = require('events')
-var util = require('util')
+var fullname1 = 'John Doe'
+var greet1 = 'Hello ' + fullname1
+var greet2 = `Hello ${ fullname1 }`
+console.log(greet1)
+console.log(greet2)
 
-function Greeter() {
-    this.gretting = 'Hello'
+var fullname2 = {
+    firstname: 'John',
+    lastname: 'Doe'
 }
-
-util.inherits(Greeter, EventEmitter)
-
-Greeter.prototype.greet = function(name) {
-    console.log(this.gretting + ' ' + name)
-    this.emit('greet', name)
-}
-
-var greeter = new Greeter()
-
-greeter.on('greet', function(name) {
-    console.log('greeted ' + name)
-})
-
-greeter.greet('Mario')
+var greet3 = 'Hello ' + fullname2
+var greet4 = `Hello ${ fullname2 }`
+console.log(greet3)
+console.log(greet4)
