8bd5fb28db77bb461a0b0a7fb7a81c9b101182c2
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-21 18:17:02 -0500

    complete module 50

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-function greet(callback) {
-    console.log('Hello from greet')
-    if (typeof callback === 'function') {
-        callback()
+var fs = require('fs')
+var greet1 = fs.readFileSync(`${__dirname}/greet.txt`, 'utf8')
+console.log(greet1)
+
+var greet2 = fs.readFile(`${__dirname}/greet.txt`, 'utf8', function(err, data) {
+    console.log(err)
+    console.log(data)
+})
+wait(2000)
+console.log('done')
+
+function wait(ms) {
+    var start = new Date().getTime()
+    var end = start
+    while (end < start + ms) {
+        end = new Date().getTime()
     }
 }
-
-greet(function() {
-    console.log('Hello from callback')
-})
-
-greet()
