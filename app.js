93627e7d673295d9317600f6cfa891961968e2ec
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-22 21:51:12 -0500

    complete module 53

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var fs = require('fs')
-var readable = fs.createReadStream(`${__dirname}/greet.txt`, { encoding: 'utf8', highWaterMark: 8 * 1024 })
+var zlib = require('zlib')
+var readable = fs.createReadStream(`${__dirname}/greet.txt`)
 var writable = fs.createWriteStream(`${__dirname}/greetcopy.txt`)
-var count = 0;
-readable.on('data', function(chunck) {
-    count++
-    console.log(`${count} ${chunck.length}`)
-    writable.write(chunck)
-    // console.log(chunck.length)
-    // console.log(chunck)
-})
+var compressed = fs.createWriteStream(`${__dirname}/greet.txt.gz`)
+readable.pipe(writable)
+readable.pipe(zlib.createGzip()).pipe(compressed)
