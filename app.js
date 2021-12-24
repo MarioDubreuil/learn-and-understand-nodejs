d1c361d3a35e7e8051415cce3509ff714e142098
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-12-24 10:54:31 -0500

    complete module 61

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ var fs = require('fs')
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    var html = fs.readFileSync(`${__dirname}/index.html`, 'utf8').replace('{Message}', 'Hello world (from Message)')
-    res.end(html)
+    fs.createReadStream(`${__dirname}/index.html`).pipe(res)
 }).listen(1337, '127.0.0.1')
