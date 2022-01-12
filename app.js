32d66d456af3c6e9490bd102b4f71f94325c5f51
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-01-11 21:33:35 -0500

    complete module 74

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
     res.send('<html><head></head><body><h1>Hello World (from express)</h1></body></html>')
 })
 
+app.get('/person/:id', function(req, res) {
+    res.send(`<html><head></head><body><h1>Hello World (from express)</h1><h2>Person:${req.params.id}</h2></body></html>`)
+})
+
 app.get('/api', function(req, res) {
     res.json({ firstname: 'John', lastname: 'Doe'})
 })
